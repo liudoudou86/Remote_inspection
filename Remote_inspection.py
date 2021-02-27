5ec74a9d8f6e5ae49b883f99e306cea55edf1de9
lz860704 <543972930@qq.com>
2021-02-27 19:31:56 +0800

    update code

--- Remote_inspection.py
+++ Remote_inspection.py
@@ -17,7 +17,7 @@ def cmd_delete():
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host,port,user,pwd)
     stdin,stdout,stderr = ssh.exec_command('rm -rf /home/BackupTiandy')
-    stdin,stdout,stderr = ssh.exec_command('rm -rf /home/script')
+    stdin,stdout,stderr = ssh.exec_command('rm -rf /home/Script')
     stdin,stdout,stderr = ssh.exec_command('rm -rf /home/BackupTiandy.zip')
     print('删除历史备份文件',stderr.read())
     ssh.close() 
@@ -29,7 +29,7 @@ def cmd_create():
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # 允许连接不在know_hosts文件中的主机
     ssh.connect(host,port,user,pwd) # 连接远程主机
     stdin,stdout,stderr = ssh.exec_command('mkdir /home/BackupTiandy') # 执行命令
-    stdin,stdout,stderr = ssh.exec_command('mkdir /home/script')
+    stdin,stdout,stderr = ssh.exec_command('mkdir /home/Script')
     print('创建备份文件夹',stderr.read())
     ssh.close() # 关闭连接
 
@@ -38,7 +38,7 @@ def sftp_put():
     ssh = paramiko.Transport((host,port))
     ssh.connect(username=user,password=pwd)
     sftp = paramiko.SFTPClient.from_transport(ssh) # 创建SFTP方法的会话连接
-    sftp.put('D:\Script.zip','/home/script/Script.zip') # PUT文件至远端服务器
+    sftp.put('D:\Remote_inspection.zip','/home/Script/Remote_inspection.zip') # PUT文件至远端服务器
     print('上传脚本文件至服务器')
     ssh.close()
 
@@ -57,13 +57,13 @@ def cmd_execute_Baq():
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host,port,user,pwd)
-    stdin,stdout,stderr = ssh.exec_command('cd /home/script/;unzip Script.zip')
+    stdin,stdout,stderr = ssh.exec_command('cd /home/Script/;unzip Remote_inspection.zip')
     print('脚本文件解压缩',stderr.read())
     stdin,stdout,stderr = ssh.exec_command('chmod 777 *')
     print('脚本文件授权',stderr.read())
-    stdin,stdout,stderr = ssh.exec_command('cd /home/script/;bash -l Backup.sh 2>&1')
+    stdin,stdout,stderr = ssh.exec_command('cd /home/Script/;bash -l Backup.sh 2>&1')
     print('Webapps和x1备份中...',stdout.read())
-    stdin,stdout,stderr = ssh.exec_command('cd /home/script/;sh BaqMysql.sh')
+    stdin,stdout,stderr = ssh.exec_command('cd /home/Script/;sh BaqMysql.sh')
     print('Mysql数据库备份中...',stderr.read())
     ssh.close()
 
@@ -73,11 +73,11 @@ def cmd_execute_Dt():
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host,port,user,pwd)
-    stdin,stdout,stderr = ssh.exec_command('cd /home/script/;unzip Script.zip')
+    stdin,stdout,stderr = ssh.exec_command('cd /home/Script/;unzip Remote_inspection.zip')
     print('脚本文件解压缩',stderr.read())
     stdin,stdout,stderr = ssh.exec_command('chmod 777 *')
     print('脚本文件授权',stderr.read())
-    stdin,stdout,stderr = ssh.exec_command('cd /home/script/;sh DtMysql.sh')
+    stdin,stdout,stderr = ssh.exec_command('cd /home/Script/;sh DtMysql.sh')
     print('Mysql数据库备份中...',stderr.read())
     ssh.close()
 
@@ -87,11 +87,11 @@ def cmd_execute_Storage():
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(host,port,user,pwd)
-    stdin,stdout,stderr = ssh.exec_command('cd /home/script/;unzip Script.zip')
+    stdin,stdout,stderr = ssh.exec_command('cd /home/Script/;unzip Remote_inspection.zip')
     print('脚本文件解压缩',stderr.read())
     stdin,stdout,stderr = ssh.exec_command('chmod 777 *')
     print('脚本文件授权',stderr.read())
-    stdin,stdout,stderr = ssh.exec_command('cd /home/script/;sh Storage.sh')
+    stdin,stdout,stderr = ssh.exec_command('cd /home/Script/;sh Storage.sh')
     print('证据图片备份中...',stderr.read())
     ssh.close()
 
@@ -130,6 +130,7 @@ while True:
         # print(user)
         pwd = str(values['_PWD_'])
         # print(pwd)
+        cmd_delete = cmd_delete()
         cmd_create = cmd_create()
         sftp_put = sftp_put()
         if values.get('_RADIO1_','True'):
@@ -142,7 +143,6 @@ while True:
             pass
         cmd_zip = cmd_zip()
         sftp_get = sftp_get()
-        cmd_delete = cmd_delete()
         sg.popup_ok('备份文件已下载至【D:\BackupTiandy.zip】',title='备份完成',font='微软雅黑')
     elif event in ['_EXIT_',None]:
         break
